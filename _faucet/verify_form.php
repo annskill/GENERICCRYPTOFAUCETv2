531e5699484542e2c4b5151b6eb072e9ae7186c7
r3wt <stuckinabox@live.com>
2013-05-21 19:41:32 -0500

    Update verify_form.php
    updated to check for input in the address area of the form

--- _faucet/verify_form.php
+++ _faucet/verify_form.php
@@ -1,14 +1,19 @@
 <?
-//This is the basic form processing file. you can see here it gets the address, ip address and the server date and time from the main "index.php" page.
+
     $address = $_POST['address'];
     $ip = $_POST['ip'];
     $timestamp = $_POST['timestamp'];
 
     $data = "$address | $ip | $timestamp ";
 
-//open the file and choose the mode. changing the name of this file is not recommended.
+    if($address==''){
+ 
+echo'No address entered. go back< your link here>';
+	}else
+//open the file and choose the mode
 $fh = fopen("list.txt", "a");	
 //writes the address and ip to the file
 fwrite($fh, $data); //close the file fclose($fh); 
-echo "Your Address Has Been Submitted <a href='your home page here'> return to the homepage?</a>";
+echo "Your Address Has Been Submitted<your link here>";
+
 ?>
